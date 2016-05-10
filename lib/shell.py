761b9b43c72e8a8ab102e861a6d1d55f90fcd6fe
Anastassios Martakos <amartako@phys.ethz.ch>
2016-05-10 14:13:14 +0200

    undone merge, added debug code

--- lib/shell.py
+++ lib/shell.py
@@ -30,13 +30,14 @@ def connectWiFi(ssid, interface, wpa):
         proc.communicate()
         proc = subprocess.Popen(['wpa_supplicant', '-B', '-i', interface, '-c', ''.join(['/etc/wpa_supplicant-', ssid, '.conf'])], stdout=subprocess.PIPE)
     else:
-        # proc = subprocess.Popen(['ip', 'link', 'set',  interface, 'up' ], stdout=subprocess.PIPE)
-        # proc.communicate()
-        # proc = subprocess.Popen(['iw', 'dev',  interface, 'scan' ], stdout=subprocess.PIPE, shell=True)
-        # proc.communicate()
-        # proc = subprocess.Popen(['iw', 'dev',  interface, 'connect', ssid ], stdout=subprocess.PIPE, shell=True)
+        proc = subprocess.Popen(['ip', 'link', 'set',  interface, 'up' ], stdout=subprocess.PIPE)
+        proc.communicate()
+        proc = subprocess.Popen(['iw', 'dev',  interface, 'scan' ], stdout=subprocess.PIPE, shell=True)
+        proc.communicate()
+        proc = subprocess.Popen(['iw', 'dev',  interface, 'connect', ssid ], stdout=subprocess.PIPE, shell=True)
+        out = decodeUTF8(proc.communicate())
+        print(out)
 
-        proc = subprocess.Popen(['ip', 'link', 'set',  interface, 'up', '||', 'iw', 'dev',  interface, 'scan', '||', 'iw', 'dev',  interface, 'connect', ssid ], stdout=subprocess.PIPE, shell=True)
 
 def getIP(interface):
     proc = subprocess.Popen(['dhclient', interface], stdout=subprocess.PIPE)
