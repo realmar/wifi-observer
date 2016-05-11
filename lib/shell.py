a058c6c9a85b8fa95e143397695ddea92b336cad
Anastassios Martakos <amartako@phys.ethz.ch>
2016-05-11 09:16:02 +0200

    typo

--- lib/shell.py
+++ lib/shell.py
@@ -32,7 +32,7 @@ def connectWiFi(ssid, interface, wpa):
     else:
         proc = subprocess.Popen(['ip', 'link', 'set',  interface, 'up' ], stdout=subprocess.PIPE)
         proc.communicate()
-        proc = subprocess.Popen(['iw', 'dev',  interface, 'scan' ], stdout=subprocess.PIPEe)
+        proc = subprocess.Popen(['iw', 'dev',  interface, 'scan' ], stdout=subprocess.PIPE)
         proc.communicate()
         proc = subprocess.Popen(['iw', 'dev',  interface, 'connect', ssid ], stdout=subprocess.PIPE)
         # out = decodeUTF8(proc.communicate())
