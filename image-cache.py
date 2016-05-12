2cc01098ae253eabeba941c16595fa9c8d9ed11a
Anastassios Martakos <amartako@phys.ethz.ch>
2016-05-12 13:35:18 +0200

    only building the plots of today, added a script to generate all plots

--- image-cache.py
+++ image-cache.py
@@ -8,29 +8,14 @@ import sys, os
 BASE_DIR = '/opt/astro-wlan-analyzer'
 sys.path.append(BASE_DIR)
 
-import subprocess
-import yaml
+import subprocess, datetime
 
-from lib.db import getAllDates
-
-# configuration
-CONFIG =  os.path.join(BASE_DIR, 'wifi-observer.conf')
-
-file = open(CONFIG, 'r')
-config = yaml.load(file)
-file.close()
-
-DB = os.path.join(BASE_DIR, config['database'])
-
-dates = getAllDates(DB)
-
-for date in dates:
-    date = date[0]
-    proc = subprocess.Popen(['mkdir', '-p', os.path.join(BASE_DIR, 'tmp')], stdout=subprocess.PIPE)
-    proc.communicate()
-    proc = subprocess.Popen(['cp', '-r', os.path.join(BASE_DIR, 'gnuplotfile'), os.path.join(BASE_DIR, 'tmp/gnuplotfile')], stdout=subprocess.PIPE)
-    proc.communicate()
-    proc = subprocess.Popen(['sed', '-i', ''.join(['s/<date>/', date, '/g']), os.path.join(BASE_DIR, 'tmp/gnuplotfile')], stdout=subprocess.PIPE)
-    proc.communicate()
-    proc = subprocess.Popen(['gnuplot', os.path.join(BASE_DIR, 'tmp/gnuplotfile')], stdout=subprocess.PIPE)
-    proc.communicate()
+date = str(datetime.date.today().year + '-' + datetime.date.today().month + '-' + datetime.date.today().day)
+proc = subprocess.Popen(['mkdir', '-p', os.path.join(BASE_DIR, 'tmp')], stdout=subprocess.PIPE)
+proc.communicate()
+proc = subprocess.Popen(['cp', '-r', os.path.join(BASE_DIR, 'gnuplotfile'), os.path.join(BASE_DIR, 'tmp/gnuplotfile')], stdout=subprocess.PIPE)
+proc.communicate()
+proc = subprocess.Popen(['sed', '-i', ''.join(['s/<date>/', date, '/g']), os.path.join(BASE_DIR, 'tmp/gnuplotfile')], stdout=subprocess.PIPE)
+proc.communicate()
+proc = subprocess.Popen(['gnuplot', os.path.join(BASE_DIR, 'tmp/gnuplotfile')], stdout=subprocess.PIPE)
+proc.communicate()
