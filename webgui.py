9c53efa39a0860700b1b72a2a1c5d3ef86b4a32b
Anastassios Martakos <amartako@phys.ethz.ch>
2016-05-12 16:38:11 +0200

    Added wsgi and apache2 support

--- webgui.py
+++ webgui.py
@@ -35,4 +35,4 @@ def getSVG(date):
     return app.send_static_file(date + '.svg')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=False)
---
+++ webgui.wsgi
@@ -0,0 +1,6 @@
+import sys, os
+
+BASE_DIR = '/opt/wifi-observer'
+sys.path.append(BASE_DIR)
+
+from webgui import app as application
